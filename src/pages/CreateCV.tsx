3f92216a3e950f65644bfea3d6e538aae70df470
Anthony Gibah <gibatekpro@gmail.com>
2024-12-06 02:14:44 +0000

    Create CV v2

--- src/pages/CreateCV.tsx
+++ src/pages/CreateCV.tsx
@@ -241,6 +241,9 @@ export const CreateCV: React.FC = () => {
         console.log(resume)
         setIsBeingEdited(resume)
         customResumeFormFormik.setValues(resume.data);
+        resumeNameFormFormik.setValues({
+            resumeName: resume?.data.resumeName || ""
+        });
     };
 
     useEffect(() => {
@@ -260,6 +263,9 @@ export const CreateCV: React.FC = () => {
         })
 
         customResumeFormFormik.setValues(DefaultResumeData);
+        resumeNameFormFormik.setValues({
+            resumeName: ""
+        });
 
     }
 
@@ -508,7 +514,7 @@ export const CreateCV: React.FC = () => {
                                             data-bs-placement="top" title="Add Section">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                                              fill="currentColor" className="bi bi-plus-lg" viewBox="0 0 16 16">
-                                            <path fill-rule="evenodd"
+                                            <path fillRule="evenodd"
                                                   d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
                                         </svg>
                                     </button>
